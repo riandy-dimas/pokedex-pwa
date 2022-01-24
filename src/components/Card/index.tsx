355cbb8fc13ce210a364f94bc5684ab173c8c46f
Riandy Dimas <riandy.dimas@shopee.com>
2022-01-25 06:35:40 +0700

    feat: update card styling

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,22 +1,26 @@
 import './styles.css'
 
+import { TPokemonType } from '../../interfaces/pokemon';
+
 type TCard = {
   name: string
   number: number
   src: string,
+  type: TPokemonType
 }
 
 const Card = ({
   name,
   number,
   src,
+  type
 }: TCard) => {
   const paddedNumber = `${number}`.padStart(3, '0');
-  return <figure className='card'>
+  return <figure className={`card card--${type}`}>
     <img className='card--image' src={src} alt={`${number}, ${name}`} />
     <div className='card--detail'>
       <figcaption className='card--name'>{ name }</figcaption>
-      <h5 className='card--number'>{paddedNumber}</h5>
+      <h5 className='card--number'>#{paddedNumber}</h5>
     </div>
   </figure>;
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,30 +2,37 @@ import './styles.css';
 
 import PokemonCard from '../../components/Card'
 import Clickable from '../../components/utility/Clickable'
+import { TPokemonType } from '../../interfaces/pokemon';
 
 const DUMMY_LIST: {
   name: string
   number: number
+  type: TPokemonType
 }[] = [
   {
     name: 'bulbasaur',
     number: 1,
+    type: 'grass',
   },
   {
     name: 'ivysaur',
     number: 2,
+    type: 'grass',
   },
   {
     name: 'venusaur',
     number: 3,
+    type: 'grass',
   },
   {
     name: 'charmander',
     number: 4,
+    type: 'fire',
   },
   {
     name: 'charmeleon',
     number: 5,
+    type: 'fire',
   },
 ]
 
@@ -37,11 +44,12 @@ const Home = () => {
     </header>
     <main className='home__content'>
       {
-        DUMMY_LIST.map(({ name, number }) => (
+        DUMMY_LIST.map(({ name, number, type }) => (
           <Clickable key={number} url={`${name}`}>
             <PokemonCard
               name={name}
               number={number}
+              type={type}
               src='https://via.placeholder.com/475'
             />
           </Clickable>
